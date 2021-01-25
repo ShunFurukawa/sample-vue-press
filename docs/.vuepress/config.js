c2097f2d0b303987168807388f65838bf15603a0
Shun Furukawa <furukawa9312@gmail.com>
2021-01-25 12:56:55 +0900

    ADD default theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  base: '/',
+  dest: './build',
   title: 'Hello VuePress',
   description: 'Just playing around',
+  locales: {
+    '/': {
+      lang: 'ja'
+    }
+  },
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: 'Foo', link: '/foo/' },
+      { text: 'Bar', link: '/bar/' }
+    ]
+  }
 };
